377cce0d0d9cb6424ccf987c39f035397fe8eb3c
jatcod3r <jullian@coder.com>
2026-08-04 00:37:14 -0700

    feat: add NodePool + Class configs

--- infra/aws/us-east-2/k8s/argo/karpenter/variables.tf
+++ infra/aws/us-east-2/k8s/argo/karpenter/variables.tf
@@ -11,6 +11,15 @@ variable "cluster_name" {
   type = string
 }
 
+variable "cluster_node_iam_role_name" {
+  type = string
+}
+
 variable "vpc_name" {
   type = string
+}
+
+variable "private_subnet_suffix" {
+  type    = string
+  default = "private"
 }
